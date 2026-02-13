5341017bfd91a118c8a67ebf77fa247bf260ae40
Benedikt Nerb <nerb.benedikt@gmail.com>
2026-02-13 14:07:47 +0100

    Checkout connman, iw, iwd, nftables and libnftnl from upstream master

--- packages/network/iwd/package.mk
+++ packages/network/iwd/package.mk
@@ -2,12 +2,12 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="iwd"
-PKG_VERSION="3.3"
-PKG_SHA256="8189e15e701112f871fb5f5e9351f007c9098754b4168ed43cc5422a3adc0255"
+PKG_VERSION="3.11"
+PKG_SHA256="85730e79de05978e26e44b958fd11e2ebb45424ca181f8db7a8daf25fc2226ea"
 PKG_LICENSE="GPL"
 PKG_SITE="https://git.kernel.org/cgit/network/wireless/iwd.git/about/"
 PKG_URL="https://www.kernel.org/pub/linux/network/wireless/iwd-${PKG_VERSION}.tar.xz"
-PKG_DEPENDS_TARGET="toolchain readline dbus"
+PKG_DEPENDS_TARGET="autotools:host gcc:host readline dbus"
 PKG_LONGDESC="Wireless daemon for Linux"
 PKG_TOOLCHAIN="autotools"
 
@@ -39,4 +39,3 @@ post_makeinstall_target() {
 post_install() {
   enable_service iwd.service
 }
-
